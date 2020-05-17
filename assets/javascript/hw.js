1062ad0c97a08096ed471a14e3464035e302cc4c
Yoon Ho Ryoo <ryoo94@gmail.com>
2020-05-16 23:10:06 -0700

    Finished js code with everything working

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,5 +1,5 @@
 // dynamic button array
-var topics = ["Avatar: The Last Airbender", "Samurai Jack", "Powerpuff Girls",  "Spongebob Squarepants", "Fairly OddParents", "Teen Titans", "Archer", "The Adventures of Jimmy Neutron", "Kim Possible", "The Boondocks"];
+var topics = ["Avatar: The Last Airbender", "Samurai Jack", "Powerpuff Girls",  "Spongebob Squarepants", "Fairly OddParents", "Teen Titans", "Archer", "Jimmy Neutron", "Kim Possible", "The Boondocks"];
 
 var button;
 var topic = "";
@@ -26,6 +26,8 @@ $("#buttonArea").on("click",".btn",function(){
 
     }).done(function(response) {
         console.log(response);
+        // this clears the page of GIFs before a new topic is selected
+        $("#gifArea").empty();
 
         var results = response.data;
 
@@ -33,14 +35,18 @@ $("#buttonArea").on("click",".btn",function(){
 
             // this will prevent GIFs rated PG-13 & R from showing
             if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                var topicDiv = $("<div>");
+                var topicDiv = $("<div class='gifs'>");
                 var rating = results[i].rating;
 
-                var p = $("<p>").text("Rating: " + results[i].rating);
+                var p = $("<p>").html("<i>Rating: </i>" + results[i].rating);
                 var topicImage = $("<img>");
 
-                topicImage.attr("src", results[i].images.fixed_height.url);
+                // this adds the 'animation' attributes onto the GIFs so it can be toggled
+                topicImage.attr("src", results[i].images.fixed_height_still.url);
                 topicImage.addClass("gif");
+                topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+                topicImage.attr("data-animate", results[i].images.fixed_height.url);
+                topicImage.attr("data-state", "still");
 
                 topicDiv.append(p);
                 topicDiv.append(topicImage);
@@ -52,7 +58,8 @@ $("#buttonArea").on("click",".btn",function(){
     });
 })
 
-$(".gif").on("click",".gif",function(){
+$("#gifArea").on("click",".gif",function(){
+    event.preventDefault();
     var state = $(this).attr("data-state");
         if(state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
@@ -63,6 +70,7 @@ $(".gif").on("click",".gif",function(){
         }
 });
 
+// this will add whatever was entered into the 'Add a Cartoon' field once submit is clicked onto the list of buttons
 $("#submitButton").on("click",function(){
     event.preventDefault();
 
