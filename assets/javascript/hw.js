a3892324284e0673106f13f4d4007cbdac4a5a13
Yoon Ho Ryoo <ryoo94@gmail.com>
2020-05-16 22:13:42 -0700

    In hw.js on line 28, response was misspelled and therefore did not function correctly

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,7 +1,8 @@
+// dynamic button array
 var topics = ["Avatar: The Last Airbender", "Samurai Jack", "Powerpuff Girls",  "Spongebob Squarepants", "Fairly OddParents", "Teen Titans", "Archer", "The Adventures of Jimmy Neutron", "Kim Possible", "The Boondocks"];
 
 var button;
-var newTopics = "";
+var topic = "";
 
 // this function will create buttons from the 'topics' array
 var createButton = function(){
@@ -14,33 +15,60 @@ var createButton = function(){
 
 $("#buttonArea").on("click",function(){
     var cartoons = $(this).attr("data");
-    var queryURL = "https://api.giphy.com/v1/gifs/search" + cartoons + "&api_key=MRdbFPL3mQjxO5iDgMSWJ1Cgls97KPfn";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cartoons + 
+        "&api_key=MRdbFPL3mQjxO5iDgMSWJ1Cgls97KPfn&limit=10";
     
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+
+    }).done(function(response) {
         console.log(response);
 
-        var results = reponse.data;
+        var results = response.data;
 
         for (var i = 0; i<results.length; i++) {
-            var topicDiv = $("<div>");
+
+            // this will prevent GIFs rated PG-13 & R from showing
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var topicDiv = $("<div>");
+                var rating = results[i].rating;
 
                 var p = $("<p>").text("Rating: " + results[i].rating);
-                
                 var topicImage = $("<img>");
 
                 topicImage.attr("src", results[i].images.fixed_height.url);
+                topicImage.addClass("gif");
 
                 topicDiv.append(p);
                 topicDiv.append(topicImage);
 
                 $("#gifArea").prepend(topicDiv);
+            }
 
+        };
+    });
+})
 
+$(".gif").on("click",function(){
+    var state = $(this).attr("data-state");
+        if(state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else{
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
         }
-    })
+});
+
+$("#submitButton").on("click",function(){
+    event.preventDefault();
+
+    topic = $("#cartoon-input").val().trim();
+    topics.push(topic);
+    console.log(topics);
+    
+    createButton();
 })
 
 createButton();
